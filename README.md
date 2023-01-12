9f2220f7e53dbe72dc6add9a8d2066664e083c7b
Dante de Lordran <dantelopez305@outlook.com>
2023-01-12 17:15:00 -0600

    Scans the name and builds a new array with random numbers but they repeat, working on unrepeatable numbers

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <h1>A simple word randomizer</h1>
 <h2>For now its just a console input/output program for randomize the letters of a word to create new random words</h2>
 <h2>For further plans I may add a graphic user interface</h2>
+<h4>1/12/2023</h4>
--- src/Randomizer/Randomizer.java
+++ src/Randomizer/Randomizer.java
@@ -1,6 +1,7 @@
 package Randomizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Randomizer {
@@ -22,7 +23,36 @@ public class Randomizer {
         for(Character c : word.toCharArray()){
             charString.add(c);
         }
-        System.out.println("" + charString);
+        listWordRandomizer(charString);
+    }
+
+    public void listWordRandomizer( List<Character> listWord){
+        char[] newList = new char[listWord.size()];
+        for(int i = 0; i < 1; i++){
+            for(int j = 0; j < listWord.size(); j++){
+                
+                Random random = new Random();
+                int[] randomNumbersArray = new int[listWord.size()];
+
+                for(int k = 0; k < listWord.size(); k++){
+                    
+                    int randomNumber = random.nextInt(listWord.size());
+                    randomNumbersArray[k] = randomNumber;
+                    
+                    
+                }
+                System.out.println(""+ randomNumbersArray[j]);
+            }
+        }
+    }
+
+    public boolean isRepeated(int[] arr, int item) {
+        for (int n : arr) {
+            if (item == n) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
