3fd4e667b57d24f9c0e48ac9ada36493241a0985
Dante de Lordran <dantelopez305@outlook.com>
2023-01-12 23:21:34 -0600

    Shuffle number under an int array

--- src/Randomizer/Randomizer.java
+++ src/Randomizer/Randomizer.java
@@ -3,6 +3,7 @@ package Randomizer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Randomizer {
@@ -16,7 +17,8 @@ public class Randomizer {
         System.out.println("Word to randomize : ");
         String word = sc.next();
         sc.close();
-        wordToList(word.toLowerCase());
+        // wordToList(word.toLowerCase());
+        wordToArray(word);
     }
 
     public void wordToList(String word) {
@@ -24,14 +26,17 @@ public class Randomizer {
         for (Character c : word.toCharArray()) {
             charString.add(c);
         }
-        //listWordRandomizer(charString);
+        // listWordRandomizer(charString);
         wordToArray(word);
     }
 
-    public void wordToArray(String word){
+    public void wordToArray(String word) {
 
         char[] wordToCharArray = new char[word.length()];
-
+        for (int i = 0; i < word.length(); i++) {
+            wordToCharArray[i] = word.charAt(i);
+        }
+        arrayWordRandomizer(wordToCharArray);
     }
 
     public void listWordRandomizer(List<Character> listWord) {
@@ -48,10 +53,38 @@ public class Randomizer {
             ArrayList<Character> charList = new ArrayList<Character>();
 
             for (int k = 0; k < listWord.size(); k++) {
-                charList.set(k, listWord.get( shuffledNumbersList.get(k) ));
+                charList.set(k, listWord.get(shuffledNumbersList.get(k)));
             }
-            System.out.println(""+ charList);
+            System.out.println("" + charList);
+        }
+    }
+
+    public void arrayWordRandomizer(char[] charWordArray) {
+        int[] auxIntArray = new int[charWordArray.length];
+
+        for (int i = 0; i < charWordArray.length; i++) {
+            auxIntArray[i] = i;
         }
 
+        shuffle(auxIntArray);
+
+        
+
+    }
+
+    Random random;
+    public void shuffle(int[] array) {
+        
+        if (random == null) random = new Random();
+        int count = array.length;
+        for (int i = count; i > 1; i--) {
+            swap(array, i - 1, random.nextInt(i));
+        }
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
