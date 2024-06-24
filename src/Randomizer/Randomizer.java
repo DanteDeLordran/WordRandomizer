b31b11142079048bc1e40b8733ac41c8560fdee1
Dante de Lordran <lordran@darsaras.dev>
2024-06-23 20:37:06 -0600

    Slightly improved

--- src/Randomizer/Randomizer.java
+++ src/Randomizer/Randomizer.java
@@ -6,28 +6,15 @@ import java.util.Scanner;
 public class Randomizer {
 
     public void init() {
-        getWord();
+        arrayWordRandomizer();
     }
 
-    public void getWord() {
+    public void arrayWordRandomizer() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Word to randomize : ");
         String word = sc.next();
         sc.close();
-        // wordToList(word.toLowerCase());
-        wordToArray(word);
-    }
-
-    public void wordToArray(String word) {
-
-        char[] wordToCharArray = new char[word.length()];
-        for (int i = 0; i < word.length(); i++) {
-            wordToCharArray[i] = word.charAt(i);
-        }
-        arrayWordRandomizer(wordToCharArray);
-    }
-
-    public void arrayWordRandomizer(char[] charWordArray) {
+        char[] charWordArray = word.toCharArray();
         int[] auxIntArray = new int[charWordArray.length];
 
         for (int i = 0; i < charWordArray.length; i++) {
@@ -47,15 +34,10 @@ public class Randomizer {
         }
     }
 
-    Random random;
-
     public void shuffle(int[] array) {
-
-        if (random == null)
-            random = new Random();
         int count = array.length;
         for (int i = count; i > 1; i--) {
-            swap(array, i - 1, random.nextInt(i));
+            swap(array, i - 1, new Random().nextInt(i));
         }
     }
 
